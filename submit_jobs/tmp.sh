16f7c945422ff9d7ac871193b83c8a7b29a5cfb2
Nikola Popovic <nipopovic@snapo.ee.ethz.ch>
2021-10-11 13:01:49 +0200

    ...

--- submit_jobs/tmp.sh
+++ submit_jobs/tmp.sh
@@ -8,7 +8,7 @@
 #BSUB -J "tmp"
 #BSUB -R lca # workaround for the current wandb cluster bug
 
-DATA_SET=birds
+DATA_SET=aircraft
 
 if [ "$DATA_SET" = "aircraft" ]; then
   ZIP_FILE_NAME=fgvc-aircraft-2013b.tar.gz

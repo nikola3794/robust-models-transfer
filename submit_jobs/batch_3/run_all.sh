0e2bb0f7422793192a47d972bb33be180b8dfdca
Nikola Popovic <nipopovic@snapo.ee.ethz.ch>
2021-10-13 14:51:58 +0200

    ...

--- submit_jobs/batch_3/run_all.sh
+++ submit_jobs/batch_3/run_all.sh
@@ -6,15 +6,15 @@ export PYTHONPATH=${PYTHONPATH}:${PROJECT_ROOT_DIR}
 cd ${PROJECT_ROOT_DIR}
 pwd
 
-bsub < submit_jobs/batch_2/aircraft_rnd_rnd.sh
-bsub < submit_jobs/batch_2/birds_rnd_rnd.sh
-bsub < submit_jobs/batch_2/caltech101_rnd_rnd.sh
-bsub < submit_jobs/batch_2/caltech256_rnd_rnd.sh
-bsub < submit_jobs/batch_2/cifar10_rnd_rnd.sh
-bsub < submit_jobs/batch_2/cifar100_rnd_rnd.sh
-bsub < submit_jobs/batch_2/dtd_rnd_rnd.sh
-bsub < submit_jobs/batch_2/flowers_rnd_rnd.sh
-bsub < submit_jobs/batch_2/food_rnd_rnd.sh
-bsub < submit_jobs/batch_2/pets_rnd_rnd.sh
-bsub < submit_jobs/batch_2/stanford_cars_rnd_rnd.sh
-bsub < submit_jobs/batch_2/SUN397_rnd_rnd.sh
+bsub < submit_jobs/batch_3/aircraft_rnd_rnd.sh
+bsub < submit_jobs/batch_3/birds_rnd_rnd.sh
+bsub < submit_jobs/batch_3/caltech101_rnd_rnd.sh
+bsub < submit_jobs/batch_3/caltech256_rnd_rnd.sh
+bsub < submit_jobs/batch_3/cifar10_rnd_rnd.sh
+bsub < submit_jobs/batch_3/cifar100_rnd_rnd.sh
+bsub < submit_jobs/batch_3/dtd_rnd_rnd.sh
+bsub < submit_jobs/batch_3/flowers_rnd_rnd.sh
+bsub < submit_jobs/batch_3/food_rnd_rnd.sh
+bsub < submit_jobs/batch_3/pets_rnd_rnd.sh
+bsub < submit_jobs/batch_3/stanford_cars_rnd_rnd.sh
+bsub < submit_jobs/batch_3/SUN397_rnd_rnd.sh

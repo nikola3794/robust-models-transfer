0ea360ab384124752fbabd0500f38028954ea2c8
Nikola Popovic <nipopovic@snapo.ee.ethz.ch>
2021-11-12 16:04:45 +0100

    ...

---
+++ submit_jobs/CVPR/round3/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set project paths
+PROJECT_ROOT_DIR=/cluster/project/cvl/specta/code/robust-models-transfer
+cd ${PROJECT_ROOT_DIR}
+pwd
+
+SCRIPT_DIR=submit_jobs/CVPR/round3
+cd ${SCRIPT_DIR}
+pwd
+
+bsub < deterministic.sh
+bsub < a01.sh
+bsub < a05.sh
+bsub < a10.sh
